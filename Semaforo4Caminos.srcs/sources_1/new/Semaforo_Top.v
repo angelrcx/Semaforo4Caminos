684ad4c97d0dab366a358aae9b320679b59c827e
angelrcx <hielorc@gmail.com>
2025-11-10 20:31:33 -0600

    TOP
    Creacion del archivo TOP

---
+++ Semaforo4Caminos.srcs/sources_1/new/Semaforo_Top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/10/2025 08:26:08 PM
+// Design Name: 
+// Module Name: Semaforo_Top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Semaforo_Top(
+
+    );
+endmodule
