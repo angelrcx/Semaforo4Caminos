09371b93eba0ca9df4ab886d5107a42ff9d7a82d
Sala313 <42200951@uaz.edu.mx>
2025-11-13 09:53:36 -0600

    Update DisColor.v

--- Semaforo4Caminos.srcs/sources_1/new/DisColor.v
+++ Semaforo4Caminos.srcs/sources_1/new/DisColor.v
@@ -29,7 +29,7 @@ always @(*) begin
     case (Estado)
         2'b00: Segmentos_Color = 7'b1100011; 
         2'b01: Segmentos_Color = 7'b0001000; 
-        2'b10: Segmentos_Color = 7'b1111017;
+        2'b10: Segmentos_Color = 7'b1111011;
         default: Segmentos_Color = 7'b1111111;
     endcase
 end
