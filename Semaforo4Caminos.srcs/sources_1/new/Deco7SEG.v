8407d77e7dfba3804fddd976d137be9fc39f504a
MinBotDaniel <160672426+MinBotDaniel@users.noreply.github.com>
2025-11-18 22:04:59 -0600

    Deco7SEG
    BCD A 7 SEGMENTOS

---
+++ Semaforo4Caminos.srcs/sources_1/new/Deco7SEG.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/18/2025 10:04:10 PM
+// Design Name: 
+// Module Name: Deco7SEG
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Deco7SEG(
+    input [3:0]in1,
+    output [6:0]Out7seg
+    );
+    
+    reg [6:0] Out;
+    
+    always@(*)
+    begin
+        case(in1)
+            4'b0000 : Out = 7'b1111110;
+            4'b0001 : Out = 7'b0110000;
+            4'b0010 : Out = 7'b1101101;
+            4'b0011 : Out = 7'b1111001;
+            4'b0100 : Out = 7'b0110011;
+            4'b0101 : Out = 7'b1011011;
+            4'b0110 : Out = 7'b1011111;
+            4'b0111 : Out = 7'b1110000;
+            4'b1000 : Out = 7'b1111111;
+            4'b1001 : Out = 7'b1111011;
+            default : Out = 7'b0000000;
+        endcase
+    end
+    assign Out7seg = ~Out;
+endmodule
